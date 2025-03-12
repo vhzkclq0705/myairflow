17ad1fca30727918c7571ddaf9af8981d6fab319
vhzkclq0705 <vhzkclq0705@naver.com>
2025-03-12 22:42:26 +0900

    myetl 구현 완료

--- dags/myetl.py
+++ dags/myetl.py
@@ -1,4 +1,3 @@
-from myairflow.parquetmanager import convert_csv_to_parquet, save_agg_csv
 from datetime import datetime, timedelta
 from airflow.models import Variable
 from airflow.models.dag import DAG
@@ -7,6 +6,19 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonVirtualenvOperator
 from airflow.utils.trigger_rule import TriggerRule
 import pendulum
+
+def convert_csv_to_parquet(timestamp):
+    import pandas as pd
+    dir_path = "/Users/joon/data/"
+    df = pd.read_csv(f"{dir_path}{timestamp}/data.csv")
+    df.to_parquet(f"{dir_path}{timestamp}/data.parquet", engine='pyarrow')
+
+def save_agg_csv(timestamp):
+    import pandas as pd
+    dir_path = "/Users/joon/data/"
+    df = pd.read_parquet(f"{dir_path}{timestamp}/data.parquet", engine='pyarrow')
+    gdf = df.groupby(["name", "value"]).size().reset_index(name="count")
+    gdf.to_csv(f"{dir_path}{timestamp}/agg.csv", index=False)
 
 # Directed Acyclic Graph
 with DAG(
