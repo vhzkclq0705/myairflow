19960feadca1bd715a2e498dbdd7a320c7469cc2
vhzkclq0705 <vhzkclq0705@naver.com>
2025-03-12 12:20:56 +0900

    디스코드 알림 파일 분리

---
+++ src/myairflow/notify.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+def send_noti(msg):
+    WEBHOOK_ID = os.getenv("DISCORD_WEBHOOK_ID")
+    WEBHOOK_TOKEN = os.getenv("DISCORD_WEBHOOK_TOKEN")
+    WEBHOOK_URL = f"https://discordapp.com/api/webhooks/{WEBHOOK_ID}/{WEBHOOK_TOKEN}"
+    
+    data = {'content': msg}
+    response = requests.post(WEBHOOK_URL, json=data)
+    
+    if response.status_code == 204:
+        print("메시지가 성공적으로 전송되었습니다.")
+    else:
+        print("메시지 전송에 실패했습니다.")
