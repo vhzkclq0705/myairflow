88470d437556ab31062a514fe679e887209b9ee7
vhzkclq0705 <vhzkclq0705@naver.com>
2025-04-08 17:33:45 +0900

    로컬 테스트 DAG

---
+++ dags/wiki_local.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.timezone import datetime
+
+DAG_ID = "wiki_meta"
+
+with DAG(
+    DAG_ID,
+    default_args={
+        "depends_on_past": True,
+        "retries": 1,
+        "retry_delay": timedelta(seconds=3)
+    },
+    max_active_runs=1,
+    max_active_tasks=5,
+    description="wiki data processing",
+    schedule="0 0 * * *",
+    start_date=datetime(2024, 1, 1),
+    end_date=datetime(2025, 4, 1),
+    catchup=True,
+    tags=["spark", "submit", "wiki"],
+) as dag:
+    SPARK_HOME = "/Users/joon/swcamp4/app/spark-3.5.1-bin-hadoop3"
+    
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    
+    merge_data = BashOperator(
+        task_id="merge_data",
+        bash_command="""
+            echo {{ data_interval_start.in_timezone('Asia/Seoul').strftime('%Y%m%dT%H%M%S') }}
+            ssh -i ~/.ssh/gcp-joon-key joon@34.47.75.78 \
+            "/home/joon/code/wiki/meta/run.sh {{ data_interval_start.in_timezone('Asia/Seoul').strftime('%Y%m%dT%H%M%S') }} /home/joon/code/wiki/meta/meta.py"
+        """
+    )
+    
+    start >> validate_data >> [merge_data, end]
+    merge_data >> end
